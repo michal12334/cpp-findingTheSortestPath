c46520ed91d7e463742f8a8550ea76d78479bd75
michal <michal1842001@gmail.com>
2020-07-09 14:17:22 +0200

    add getPosition and setPosition methods to Frame class

--- frame.cpp
+++ frame.cpp
@@ -5,9 +5,29 @@ using namespace sf;
 Frame::Frame() {
 }
 
+void Frame::setPosition(Vector2f position) {
+	this->position = position;
+	this->topShape.setPosition(this->position);
+	this->leftShape.setPosition(this->position);
+
+	Vector2f rightPosition;
+	rightPosition.y = this->position.y;
+	rightPosition.x = this->position.x + this->squareSize - this->frameSize;
+	this->rightShape.setPosition(rightPosition);
+
+	Vector2f bottomPosition;
+	bottomPosition.x = this->position.x;
+	bottomPosition.y = this->position.y + this->squareSize - this->frameSize;
+	this->bottomShape.setPosition(bottomPosition);
+}
+
+Vector2f Frame::getPosition() {
+	return this->position;
+}
+
 void Frame::draw(RenderTarget &target, RenderStates state) const {
-	target.draw(topShape, state);
-	target.draw(bottomShape, state);
-	target.draw(leftShape, state);
-	target.draw(rightShape, state);
+	target.draw(this->topShape, state);
+	target.draw(this->bottomShape, state);
+	target.draw(this->leftShape, state);
+	target.draw(this->rightShape, state);
 }
--- frame.h
+++ frame.h
@@ -5,12 +5,14 @@
 class Frame :public sf::Drawable {
 public:
 	Frame();
+	void setPosition(sf::Vector2f position);
+	sf::Vector2f getPosition();
 
 private:
 	void draw(sf::RenderTarget &target, sf::RenderStates state) const override;
 	sf::Vector2f position;
-	sf::Vector2f squareSize;
-	sf::Vector2f frameSize;
+	float squareSize;
+	float frameSize;
 	sf::RectangleShape topShape;
 	sf::RectangleShape bottomShape;
 	sf::RectangleShape leftShape;
