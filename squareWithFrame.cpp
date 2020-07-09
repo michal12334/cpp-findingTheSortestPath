3545e67253c3ae8eb66f8c8a1b694ead05d42799
michal <michal1842001@gmail.com>
2020-07-09 17:48:24 +0200

    add getters and setters in SquareWithFrame class for variables: position, squareSize, squareColor, frameColor, frameSize

--- squareWithFrame.cpp
+++ squareWithFrame.cpp
@@ -5,6 +5,53 @@ using namespace sf;
 SquareWithFrame::SquareWithFrame() {
 }
 
+void SquareWithFrame::setPosition(Vector2f position) {
+	this->position = position;
+	this->shape.setPosition(position);
+	this->frame.setPosition(position);
+}
+
+Vector2f SquareWithFrame::getPosition() {
+	return this->position;
+}
+
+void SquareWithFrame::setSquareSize(float size) {
+	this->squareSize = size;
+	this->shape.setSize(Vector2f{size, size});
+	this->frame.setSquareSize(size);
+}
+
+float SquareWithFrame::getSquareSize() {
+	return this->squareSize;
+}
+
+void SquareWithFrame::setSquareColor(Color color) {
+	this->squareColor = color;
+	this->shape.setFillColor(color);
+}
+
+Color SquareWithFrame::getSquareColor() {
+	return this->squareColor;
+}
+
+void SquareWithFrame::setFrameColor(Color color) {
+	this->frameColor = color;
+	this->frame.setColor(color);
+}
+
+Color SquareWithFrame::getFrameColor() {
+	return this->frameColor;
+}
+
+void SquareWithFrame::setFrameSize(float size) {
+	this->frameSize = size;
+	this->frame.setFrameSize(size);
+}
+
+float SquareWithFrame::getFrameSize() {
+	return this->frameSize;
+}
+
 void SquareWithFrame::draw(RenderTarget &target, RenderStates state) const {
 	target.draw(this->shape, state);
 	target.draw(this->frame, state);
--- squareWithFrame.h
+++ squareWithFrame.h
@@ -6,12 +6,22 @@
 class SquareWithFrame :public sf::Drawable {
 public:
 	SquareWithFrame();
+	void setPosition(sf::Vector2f position);
+	sf::Vector2f getPosition();
+	void setSquareSize(float size);
+	float getSquareSize();
+	void setSquareColor(sf::Color color);
+	sf::Color getSquareColor();
+	void setFrameColor(sf::Color color);
+	sf::Color getFrameColor();
+	void setFrameSize(float size);
+	float getFrameSize();
 
 private:
 	void draw(sf::RenderTarget &target, sf::RenderStates state) const override;
 	Frame frame;
 	sf::Vector2f position;
-	float size;
+	float squareSize;
 	sf::Color squareColor;
 	sf::Color frameColor;
 	float frameSize;
